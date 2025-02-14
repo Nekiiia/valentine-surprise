d4a1d08590b1d7f77c8e2bdd640657d121ea0e47
Nekiiia <88255035+Nekiiia@users.noreply.github.com>
2025-02-14 07:16:06 +0000

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "out",
-  images: { unoptimized: true },
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, 
+  basePath: "/valentine-surprise", 
+  assetPrefix: "/valentine-surprise/", 
 };
 
 module.exports = nextConfig;
